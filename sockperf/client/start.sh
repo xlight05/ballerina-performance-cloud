7d43a617356b6bb6ade4e73f6bc8674947aaadd6
Anjana Supun <anjanas@wso2.com>
2021-12-09 13:13:21 +0530

    Add sockperf server

---
+++ sockperf/client/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+# Copyright 2021 WSO2 Inc. (http://wso2.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------------------------------------------------------
+# Start script for sockperf cleint
+# ----------------------------------------------------------------------------
+set -e
+
+cluster_ip="${CLUSTER_IP}"
+
+sed '/bal\.perf\.test/d' /etc/hosts | sudo tee /etc/hosts
+echo "$cluster_ip bal.perf.test" | sudo tee -a /etc/hosts
+echo "hosts file"
+cat /etc/hosts
+
+sockperf ping-pong -i 10.240.0.163 -t 60 --msg-size 1400 –tcp
+
